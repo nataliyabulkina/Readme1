298ab438e1f8d4b4b479a8b00ee4ac2b6fc9c7be
Name <your.Nataliyabulkina1410@gmail.com>
2022-10-31 11:43:47 +0300

    README1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача.   Написать программу, которая из имеющегося массива строк формирует массив строк, 
+длинна которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+лучше обойтись исключительно массивами.
+*/
+namespace Massword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Массив строк";
+            string[] s = new string [100];
+            int n;
+            Console.WriteLine("Задайте количество элементов : ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                s[i] = Console.ReadLine();
+            }
+            Console.WriteLine ("Массив:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write (s[i] + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
